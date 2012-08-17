924c5224b053cee6eb5fa94a29b0626550eb97fc
bugadani <bugadani@bugadani-inspiron>
2012-08-17 18:56:38 +0200

    Update an old header. Minor changes preparing to implement inserting/deleting/updating related data

--- Parts/Table.php
+++ Parts/Table.php
@@ -2,26 +2,9 @@
 
 /**
  * This file is part of the Miny framework.
+ * (c) Dániel Buga <daniel@bugadani.hu>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version accepted by the author in accordance with section
- * 14 of the GNU General Public License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @package   Miny/Modules/ORM/Parts
- * @copyright 2012 Dániel Buga <daniel@bugadani.hu>
- * @license   http://www.gnu.org/licenses/gpl.txt
- *            GNU General Public License
- * @version   1.0-dev
+ * For licensing information see the LICENSE file.
  */
 
 namespace Modules\ORM\Parts;
@@ -96,19 +79,18 @@ class Table implements ArrayAccess, Iterator
 
     public function insert(array $data)
     {
-        $data = array_intersect_key($data, array_flip($this->descriptor->fields));
+        $record_data = array_intersect_key($data, array_flip($this->descriptor->fields));
         $fields = array();
-        foreach (array_keys($data) as $key) {
+        foreach (array_keys($record_data) as $key) {
             $fields[$key] = ':' . $key;
         }
         $placeholders = implode(', ', $fields);
-        $fields = implode(', ', array_keys($fields));
+        $field_list = implode(', ', array_keys($fields));
 
         $pattern = 'INSERT INTO %s (%s) VALUES (%s)';
-        $sql = sprintf($pattern, $this->getTableName(), $fields, $placeholders);
-        $stmt = $this->manager->connection->prepare($sql);
-        $stmt->execute($data);
-        //todo: related beszúrás
+        $sql = sprintf($pattern, $this->getTableName(), $field_list, $placeholders);
+        $this->manager->connection->prepare($sql)->execute($record_data);
+        //TODO: insert related rows if necessary
     }
 
     public function update($pk, array $data)
@@ -122,9 +104,8 @@ class Table implements ArrayAccess, Iterator
         $data['pk'] = $pk;
 
         $sql = sprintf($pattern, $this->getTableName(), implode(', ', $fields), $this->getPrimaryKey());
-        $stmt = $this->manager->connection->prepare($sql);
-        $stmt->execute($data);
-        //todo: related update
+        $this->manager->connection->prepare($sql)->execute($data);
+        //TODO: update related rows if necessary
     }
 
     public function delete($pk)
@@ -138,9 +119,8 @@ class Table implements ArrayAccess, Iterator
         $pattern = 'DELETE FROM %s WHERE %s';
 
         $sql = sprintf($pattern, $this->getTableName(), $condition);
-        $stmt = $this->manager->connection->prepare($sql);
-        $stmt->execute($parameters);
-        //TODO: delete rows with HAS relation
+        $this->manager->connection->prepare($sql)->execute($parameters);
+        //TODO: delete related rows if necessary
     }
 
     public function offsetExists($offset)
