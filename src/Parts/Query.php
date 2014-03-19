5538b83549b410427f5c4c40fe62876dc0da87be
Dániel Buga <bugadani@gmail.com>
2014-03-19 18:33:13 +0100

    Cleanups.

--- src/Parts/Query.php
+++ src/Parts/Query.php
@@ -14,27 +14,24 @@ use Iterator;
 use Modules\DBAL\Driver\Statement;
 use Modules\DBAL\QueryBuilder\Select;
 use Modules\ORM\Manager;
-use PDO;
 
 class Query implements Iterator, Countable
 {
     private $table;
     private $with = array();
     private $columns = array();
-    private $selectedExtraFields = array();
     private $where;
     private $where_params = array();
     private $having;
     private $having_params = array();
-    private $order;
-    private $group;
-    private $limit;
-    private $offset;
-    private $lock = false;
     private $rows = array();
     private $query;
     private $queryBuilder;
-    private $select;
+
+    /**
+     * @var ResultProcessor
+     */
+    private $processor;
 
     /**
      *
@@ -45,6 +42,8 @@ class Query implements Iterator, Countable
         $this->table        = $table;
         $this->manager      = $table->manager;
         $this->queryBuilder = $table->manager->connection->getQueryBuilder();
+
+        $this->processor = new ResultProcessor($table);
 
         $tableName    = $table->getTableName();
         $this->select = $this->queryBuilder
@@ -63,10 +62,10 @@ class Query implements Iterator, Countable
             unset($this->rows);
             unset($this->query);
         }
-        if (empty($this->with)) {
-            $this->with = func_get_args();
-        } else {
-            $this->with = array_merge($this->with, func_get_args());
+
+        foreach (func_get_args() as $relatedName) {
+            $this->with[] = $relatedName;
+            $this->addRelationToQuery($relatedName);
         }
 
         return $this;
@@ -178,6 +177,7 @@ class Query implements Iterator, Countable
         }
         $this->select->setFirstResult($offset);
         $this->select->setMaxResults($limit);
+        $this->processor->setLimits($limit, $offset);
 
         return $this;
     }
@@ -232,27 +232,16 @@ class Query implements Iterator, Countable
             return $this->query;
         }
 
-        $tableName = $this->table->getTableName();
-        $select    = $this->select;
-
         if (!empty($this->with)) {
+            $tableName       = $this->table->getTableName();
             $tableDescriptor = $this->table->descriptor;
 
-            $this->addSelectedColumns($select, $tableDescriptor, $tableName);
-
-            foreach ($this->with as $name) {
-                $this->addRelationToQuery(
-                    $select,
-                    $tableDescriptor,
-                    $name,
-                    $tableName
-                );
-            }
+            $this->addSelectedColumns($this->select, $tableDescriptor, $tableName);
         } else {
-            $select->select($this->columns ? : '*');
+            $this->select->select($this->columns ? : '*');
         }
 
-        $this->query = $select->get();
+        $this->query = $this->select->get();
 
         return $this->query;
     }
@@ -263,6 +252,7 @@ class Query implements Iterator, Countable
         $tableName
     ) {
         $columns     = array();
+        $extra       = array();
         $tableFields = $tableDescriptor->fields;
         foreach ($this->columns ? : $tableFields as $name) {
             if (strpos($name, '(') === false) {
@@ -277,18 +267,16 @@ class Query implements Iterator, Countable
             }
             if (!in_array($name, $tableFields) && strpos($name, ' as ')) {
                 list(, $alias) = explode(' as ', $name, 2);
-                $this->selectedExtraFields[$alias] = $alias;
+                $extra[$alias] = $alias;
             }
         }
+
+        $this->processor->setExtraFieldsForMainTable($extra);
         $select->addSelect($columns);
     }
 
-    private function addRelationToQuery(
-        Select $select,
-        TableDescriptor $tableDescriptor,
-        $relatedName,
-        $tableName
-    ) {
+    private function addRelationToQuery($relatedName)
+    {
         if (is_array($relatedName)) {
             $condition     = $relatedName;
             $relatedName   = array_shift($condition);
@@ -297,72 +285,74 @@ class Query implements Iterator, Countable
             $joinCondition = '';
         }
 
-        $primaryKey = $tableDescriptor->primary_key;
+        $related           = $this->table->getRelatedTable($relatedName);
+        $relatedTableName  = $related->getTableName();
+        $relatedTableAlias = $related->descriptor->name;
+        $relatedPrimaryKey = $related->descriptor->primary_key;
 
-        $related      = $this->table->getRelatedTable($relatedName);
-        $descriptor   = $related->descriptor;
-        $relatedTable = $related->getTableName();
-
-        $foreignKey        = $this->table->getForeignKey($relatedName);
-        $relatedPrimaryKey = $descriptor->primary_key;
-        foreach ($descriptor->fields as $field) {
-            $column = sprintf(
-                '%s.%s as %s_%s',
-                $relatedTable,
-                $field,
-                $descriptor->name,
-                $field
+        foreach ($related->descriptor->fields as $field) {
+            $this->select->addSelect(
+                sprintf(
+                    '%s.%s as %s_%s',
+                    $relatedTableName,
+                    $field,
+                    $relatedTableAlias,
+                    $field
+                )
             );
-            $select->addSelect($column);
         }
 
+        $tableName       = $this->table->getTableName();
+        $tableDescriptor = $this->table->descriptor;
         switch ($tableDescriptor->getRelation($relatedName)) {
             case TableDescriptor::RELATION_MANY_MANY:
-                $joinTable = $this->table->getJoinTable($relatedName);
 
-                $condition = sprintf(
+                $joinTable     = $this->table->getJoinTable($relatedName);
+                $leftCondition = sprintf(
                     '%s.%s = %s.%s',
                     $tableName,
-                    $primaryKey,
+                    $tableDescriptor->primary_key,
                     $joinTable,
                     $this->table->getForeignKey($tableDescriptor->name)
                 );
-                $select->leftJoin($tableName, $joinTable, null, $condition);
 
-                $condition = sprintf(
+                $rightCondition = sprintf(
                     '%s.%s = %s.%s',
                     $joinTable,
-                    $foreignKey,
-                    $relatedTable,
+                    $this->table->getForeignKey($relatedName),
+                    $relatedTableName,
                     $relatedPrimaryKey
                 );
-                $select->leftJoin($joinTable, $relatedTable, null, $condition);
 
+                $this->select->leftJoin($tableName, $joinTable, null, $leftCondition);
+                $this->select->leftJoin($joinTable, $relatedTableName, null, $rightCondition);
                 break;
+
             case TableDescriptor::RELATION_HAS:
-                $related_foreign = $this->table->getForeignKey($tableName);
+                $relatedTableForeignKey = $this->table->getForeignKey($tableName);
 
                 $condition = sprintf(
                     '%s.%s = %s.%s',
                     $tableName,
-                    $primaryKey,
-                    $relatedTable,
-                    $related_foreign,
+                    $tableDescriptor->primary_key,
+                    $relatedTableName,
+                    $relatedTableForeignKey,
                     $joinCondition
                 );
-                $select->leftJoin($tableName, $relatedTable, null, $condition);
+                $this->select->leftJoin($tableName, $relatedTableName, null, $condition);
                 break;
+
             case TableDescriptor::RELATION_BELONGS_TO:
 
                 $condition = sprintf(
                     '%s.%s = %s.%s',
                     $tableName,
-                    $foreignKey,
-                    $relatedTable,
+                    $this->table->getForeignKey($relatedName),
+                    $relatedTableName,
                     $relatedPrimaryKey,
                     $joinCondition
                 );
-                $select->leftJoin($tableName, $relatedTable, null, $condition);
+                $this->select->leftJoin($tableName, $relatedTableName, null, $condition);
                 break;
         }
     }
@@ -376,25 +366,34 @@ class Query implements Iterator, Countable
     {
         $query = $this->getQuery();
         $this->manager->log('Executing query: %s', $query);
+
         $stmt = $this->manager->connection->prepare($query);
         $this->bindParameters($stmt, $this->manager);
-        if (isset($this->limit)) {
-            $single = $this->limit == 1;
-        } elseif ($single) {
-            $this->limit = 1;
-        }
-        $this->manager->log('%s row requested', ($single ? 'Single' : 'Multiple'));
+
         $stmt->execute();
-        if ($stmt->rowCount() == 0) {
-            $this->manager->log('Results: 0');
+        if ($single) {
+            $this->processor->setLimits(1);
+            $this->manager->log('Single row requested');
 
-            return $single ? false : array();
+            if ($stmt->rowCount() == 0) {
+                $this->manager->log('Results: 0');
+                return false;
+            }
+        } else {
+            $this->manager->log('Multiple rows requested');
+
+            if ($stmt->rowCount() == 0) {
+                $this->manager->log('Results: 0');
+                return array();
+            }
         }
+
+
         if (empty($this->with)) {
-            return $this->processResults($single, $stmt, $this->manager);
+            return $this->processor->processResults($stmt);
         }
 
-        return $this->processResultsWithRelatedRecords($stmt, $single);
+        return $this->processor->processResultsWithRelatedRecords($stmt, $this->with);
     }
 
     /**
@@ -416,199 +415,6 @@ class Query implements Iterator, Countable
         if (count($params)) {
             $orm->log('Query parameters: "%s"', implode('", "', $params));
         }
-    }
-
-    /**
-     * @param bool      $single
-     * @param Statement $stmt
-     * @param Manager   $orm
-     *
-     * @return array|bool|Row
-     */
-    protected function processResults($single, Statement $stmt, $orm)
-    {
-        $rows = $stmt->fetchAll();
-        $orm->log('Results: %d', count($rows));
-        if (empty($rows)) {
-            return $single ? false : array();
-        }
-        if ($single) {
-            return new Row($this->table, current($rows));
-        }
-        $return  = array();
-        $pkField = $this->table->getPrimaryKey();
-        foreach ($rows as $row) {
-            if (isset($row[$pkField])) {
-                $return[$row[$pkField]] = new Row($this->table, $row);
-            } else {
-                $return[] = new Row($this->table, $row);
-            }
-        }
-
-        return $return;
-    }
-
-    /**
-     * @param Statement $statement
-     * @param bool      $single
-     *
-     * @return array
-     */
-    private function processResultsWithRelatedRecords(Statement $statement, $single)
-    {
-        $descriptor = $this->table->descriptor;
-        $table      = $descriptor->name;
-
-        $table_fields = array();
-        foreach ($descriptor->fields as $name) {
-            $table_fields[$name] = $table . '_' . $name;
-        }
-
-        $relation_data     = $this->getRelationData($descriptor);
-        $pk_field          = $descriptor->primary_key;
-        $records           = array();
-        $last_pk           = null;
-        $relation_last_pks = array();
-        $row_num           = 0;
-        $fetched           = 0;
-
-        $query_fields = array_merge($table_fields, $this->selectedExtraFields);
-        $row_skipped  = false;
-
-        //We fetch rows one-by-one because MANY_MANY relation type cannot be limited by LIMIT
-        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-            if ($last_pk !== $row[$table_fields[$pk_field]]) {
-                $last_pk = $row[$table_fields[$pk_field]];
-
-                if (isset($this->offset)) {
-                    $row_skipped = ($row_num++ < $this->offset);
-                }
-                if (!$row_skipped) {
-                    if (isset($this->limit) && $fetched++ == $this->limit) {
-                        break;
-                    }
-                    $records[$last_pk] = new Row($this->table, $this->getFieldsFromRow(
-                        $row,
-                        $query_fields
-                    ));
-                    $relation_last_pks = array();
-                }
-            }
-
-            if (!$row_skipped) {
-                $relation_last_pks = $this->processRelatedRecords(
-                    $relation_data,
-                    $records[$last_pk],
-                    $row,
-                    $relation_last_pks
-                );
-            }
-        }
-        $statement->closeCursor();
-        $this->table->manager->log('Results: %d', count($records));
-        if ($single) {
-            $records = current($records);
-        }
-
-        return $records;
-    }
-
-    /**
-     * @param $relation_data
-     * @param $return
-     * @param $row
-     * @param $relation_last_pks
-     *
-     * @return mixed
-     */
-    private function processRelatedRecords($relation_data, $return, $row, $relation_last_pks)
-    {
-        foreach ($this->with as $name) {
-            if (is_array($name)) {
-                $name = $name[0];
-            }
-            $relation_type     = $relation_data[$name]['type'];
-            $relation_table    = $relation_data[$name]['table'];
-            $relation_pk_alias = $relation_data[$name]['primary_key_alias'];
-            $relation_fields   = $relation_data[$name]['fields'];
-
-            if ($relation_type !== TableDescriptor::RELATION_BELONGS_TO) {
-                if (!isset($return->$name)) {
-                    $return->$name = array();
-                }
-            }
-
-            if ($row[$relation_pk_alias]) {
-                $relation_pk_value = $row[$relation_pk_alias];
-            } else {
-                continue;
-            }
-
-            if (isset($relation_last_pks[$name]) && $relation_last_pks[$name] == $relation_pk_value) {
-                //This row is present multiple times and we have already processed it.
-                continue;
-            }
-
-            $relation_last_pks[$name] = $relation_pk_value;
-            $relation_row             = new Row($relation_table, $this->getFieldsFromRow(
-                $row,
-                $relation_fields
-            ));
-            if ($relation_type == TableDescriptor::RELATION_BELONGS_TO) {
-                $return->$name = $relation_row;
-            } else {
-                $var                     = & $return->$name;
-                $var[$relation_pk_value] = $relation_row;
-            }
-        }
-
-        return $relation_last_pks;
-    }
-
-    /**
-     * @param TableDescriptor $descriptor
-     *
-     * @return array
-     */
-    private function getRelationData(TableDescriptor $descriptor)
-    {
-        $relation_data = array();
-        foreach ($this->with as $name) {
-            if (is_array($name)) {
-                $name = $name[0];
-            }
-            $relation_table       = $this->table->getRelatedTable($name);
-            $relation_data[$name] = array(
-                'fields' => array(),
-                'table'  => $relation_table,
-                'type'   => $descriptor->getRelation($name),
-            );
-            foreach ($relation_table->descriptor->fields as $field) {
-                $relation_data[$name]['fields'][$field] = $name . '_' . $field;
-            }
-            $primaryKey                                = $relation_table->getPrimaryKey();
-            $relation_data[$name]['primary_key_alias'] = $relation_data[$name]['fields'][$primaryKey];
-        }
-
-        return $relation_data;
-    }
-
-    /**
-     * @param array $row
-     * @param array $fields
-     *
-     * @return array
-     */
-    private function getFieldsFromRow(array $row, array $fields)
-    {
-        $rowdata = array();
-        foreach ($fields as $field => $alias) {
-            if (isset($row[$alias])) {
-                $rowdata[$field] = $row[$alias];
-            }
-        }
-
-        return $rowdata;
     }
 
     /**
