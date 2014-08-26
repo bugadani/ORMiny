d056c1ab910686f2c53e967aab4501779dd28285
Buga Dániel <bugadani@gmail.com>
2014-08-27 00:50:43 +0200

    Add Miny module class.

---
+++ src/Module.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the Miny framework.
+ * (c) Dániel Buga <daniel@bugadani.hu>
+ *
+ * For licensing information see the LICENSE file.
+ */
+
+namespace Modules\ORM;
+
+use Miny\Application\BaseApplication;
+
+class Module extends \Miny\Modules\Module
+{
+    public function defaultConfiguration()
+    {
+        return [
+            'entityMap' => []
+        ];
+    }
+
+    public function init(BaseApplication $app)
+    {
+        $entityManager = $app->getContainer()->get(__NAMESPACE__ . '\\EntityManager');
+        foreach ($this->getConfiguration('entityMap') as $entityName => $className) {
+            $entityManager->register($entityName, $className);
+        }
+    }
+}
