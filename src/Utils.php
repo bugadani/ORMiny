7d543be7424ec77992e13242d50b4383c6bee8a6
Dániel Buga <bugadani@gmail.com>
2015-08-10 15:19:05 +0200

    Remove 5.6 specific array_filter arguments

--- src/Utils.php
+++ src/Utils.php
@@ -21,37 +21,42 @@ class Utils
         };
     }
 
-    public static function filterPrefixedElements(array $elements, $prefix, $flag = 0)
+    public static function filterPrefixedElements(array $unfiltered, $prefix, $flag = 0)
     {
         $useKeys      = $flag & Utils::FILTER_USE_KEYS;
         $removePrefix = $flag & Utils::FILTER_REMOVE_PREFIX;
 
-        $elements = array_filter(
-            $elements,
-            Utils::createStartWithFunction($prefix),
-            $useKeys ? ARRAY_FILTER_USE_KEY : 0
-        );
-
-        if ($removePrefix) {
-            $prefixLength = strlen($prefix);
-            $record       = [];
-
-            if ($useKeys) {
-                foreach ($elements as $key => $value) {
-                    $key            = substr($key, $prefixLength);
-                    $record[ $key ] = $value;
-                }
-            } else {
-                foreach ($elements as $key => $value) {
-                    $value          = substr($value, $prefixLength);
-                    $record[ $key ] = $value;
+        $startsWithPrefix = Utils::createStartWithFunction($prefix);
+        if ($useKeys) {
+            $filtered = [];
+            foreach ($unfiltered as $key => $value) {
+                if ($startsWithPrefix($key)) {
+                    $filtered[ $key ] = $value;
                 }
             }
-
-            return $record;
         } else {
-            return $elements;
+            $filtered = array_filter($unfiltered, $startsWithPrefix);
         }
+        if (!$removePrefix) {
+            return $filtered;
+        }
+
+        $prefixLength = strlen($prefix);
+
+        $filteredAndStripped = [];
+        if ($useKeys) {
+            foreach ($filtered as $key => $value) {
+                $key                         = substr($key, $prefixLength);
+                $filteredAndStripped[ $key ] = $value;
+            }
+        } else {
+            foreach ($filtered as $key => $value) {
+                $value                       = substr($value, $prefixLength);
+                $filteredAndStripped[ $key ] = $value;
+            }
+        }
+
+        return $filteredAndStripped;
     }
 
     public static function notNull($value)
